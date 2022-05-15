6176629f2e7cd6b4fb598866e417ba7791a154e6
gerasaka <gerryjulioo@gmail.com>
2022-05-15 12:28:10 +0700

    fix: add `useRef` hooks to input tag to delete focus state on input field when form submitted

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './style.module.css';
 
 interface Props {
@@ -8,12 +8,21 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className={styles.input} onSubmit={handleAdd}>
+    <form
+      className={styles.input}
+      onSubmit={e => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
         type='text'
         placeholder='Enter your task'
         className={styles.input__box}
+        ref={inputRef}
         value={todo}
         onChange={e => setTodo(e.target.value)}
       />

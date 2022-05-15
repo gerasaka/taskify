fb9a1961964ffb9cbfb19fca0a38f9fb0ed783ee
gerasaka <gerryjulioo@gmail.com>
2022-05-15 11:04:01 +0700

    feat: pass `todo` state to `InputField` component

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from './style.module.css';
 
-const index: React.FC = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className={styles.input}>
       <input
         type='text'
         placeholder='Enter your task'
         className={styles.input__box}
+        value={todo}
+        onChange={e => setTodo(e.target.value)}
       />
       <button className={styles.input_submit} type='submit'>
         Go
@@ -16,4 +23,4 @@ const index: React.FC = () => {
   );
 };
 
-export default index;
+export default InputField;

d46dd90a21462d96993d22143fec9226635b9919
gerasaka <gerryjulioo@gmail.com>
2022-05-15 15:28:19 +0700

    feat: create handler when todo edited

--- src/components/TodoCard/index.tsx
+++ src/components/TodoCard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Todo } from '../../model';
 import style from './style.module.css';
 import { FiEdit3 } from 'react-icons/fi';
@@ -12,6 +12,9 @@ interface Props {
 }
 
 const TodoCard: React.FC<Props> = ({ todo, todoList, setTodoList }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   const handleDone = (id: number) => {
     setTodoList(
       todoList.map(todo =>
@@ -24,15 +27,40 @@ const TodoCard: React.FC<Props> = ({ todo, todoList, setTodoList }) => {
     setTodoList(todoList.filter(todo => todo.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodoList(
+      todoList.map(todo =>
+        todo.id === id ? { ...todo, todo: editTodo } : todo
+      )
+    );
+    setEdit(false);
+  };
+
   return (
-    <form className={style.todo__single}>
-      {todo.isDone ? (
+    <form className={style.todo__single} onSubmit={e => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          value={editTodo}
+          className={style.todo__single_text}
+          onChange={e => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
         <s className={style.todo__single_text}>{todo.todo}</s>
       ) : (
         <span className={style.todo__single_text}>{todo.todo}</span>
       )}
+
       <div>
-        <span className={style.icon}>
+        <span
+          className={style.icon}
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <FiEdit3 />
         </span>
         <span className={style.icon} onClick={() => hanleDelete(todo.id)}>
